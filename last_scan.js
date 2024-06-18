ec7e7b8727996f3150bed6c0da08e9688d11eb82
mkoch-tud <>
2024-06-18 14:15:57 +0200

    automated update of weekly scan results

--- last_scan.js
+++ last_scan.js
@@ -1,2 +1,2 @@
 var last_scan = '2024-06-14';
-var num_odns = '1.909.744';
+var num_odns = '1,909,744';
--- tcp_last_scan.js
+++ tcp_last_scan.js
@@ -1,2 +1,2 @@
 var last_scan = '2024-06-09';
-var num_odns = '.948.703';
+var num_odns = '948,703';
