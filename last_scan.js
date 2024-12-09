c5df7d076dabe0a181ba90c65f8a53ea15754b6b
mkoch-tud <>
2024-12-09 08:00:02 +0100

    automated update of weekly scan results

--- last_scan.js
+++ last_scan.js
@@ -1,2 +1,2 @@
-var last_scan = '2024-11-29';
-var num_odns = '1,733,026';
+var last_scan = '2024-12-06';
+var num_odns = '1,733,114';
--- tcp_last_scan.js
+++ tcp_last_scan.js
@@ -1,2 +1,2 @@
-var last_scan = '2024-11-30';
-var num_odns = '928,867';
+var last_scan = '2024-12-07';
+var num_odns = '930,419';
