4cd400475500ebd2eb0964c255b0ebfa0fa73ef6
mkoch-tud <>
2026-06-01 20:00:03 +0200

    automated update of weekly scan results

--- last_scan.js
+++ last_scan.js
@@ -1,2 +1,2 @@
-var last_scan = '2026-05-22';
-var num_odns = '1,411,895';
+var last_scan = '2026-05-29';
+var num_odns = '1,460,700';
--- tcp_last_scan.js
+++ tcp_last_scan.js
@@ -1,2 +1,2 @@
-var last_scan = '2026-05-24';
-var num_odns = '811,744';
+var last_scan = '2026-05-31';
+var num_odns = '820,257';
