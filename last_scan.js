513b15bafe133b00ea0232e0dc3eda786b52bfbe
mkoch-tud <>
2026-07-13 20:00:02 +0200

    automated update of weekly scan results

--- last_scan.js
+++ last_scan.js
@@ -1,2 +1,2 @@
-var last_scan = '2026-07-03';
-var num_odns = '1,417,892';
+var last_scan = '2026-07-10';
+var num_odns = '1,412,093';
--- tcp_last_scan.js
+++ tcp_last_scan.js
@@ -1,2 +1,2 @@
-var last_scan = '2026-07-05';
-var num_odns = '817,729';
+var last_scan = '2026-07-12';
+var num_odns = '822,458';
